92d5156273bf07f6d6ad843ef8d7f7727a31911a
Tanaka Chingonzo <tanaching@gmail.com>
2021-01-19 15:26:05 -0500

    fixed security stuff

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -1,14 +1,14 @@
-import firebase from "firebase";
+import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCXt3qTLzyR2fmzG_3LX09Aw5ekTVEm0Bw",
-    authDomain: "fbx-clone.firebaseapp.com",
-    databaseURL: "https://fbx-clone.firebaseio.com",
-    projectId: "fbx-clone",
-    storageBucket: "fbx-clone.appspot.com",
-    messagingSenderId: "622305207300",
-    appId: "1:622305207300:web:2437eda7f729eaa0670151",
-    measurementId: "G-C5HQ374H47"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: 'fbx-clone.firebaseapp.com',
+  databaseURL: 'https://fbx-clone.firebaseio.com',
+  projectId: 'fbx-clone',
+  storageBucket: 'fbx-clone.appspot.com',
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: MEASUREMENT_ID,
 });
 
 const db = firebaseApp.firestore();
