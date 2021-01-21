34f69d63740d4c191ddb91bd58ca68e9f41bcf1c
Tanaka Chingonzo <tanaching@gmail.com>
2021-01-21 12:20:50 -0500

    fixed API

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: 'fbx-clone.firebaseapp.com',
   databaseURL: 'https://fbx-clone.firebaseio.com',
   projectId: 'fbx-clone',
   storageBucket: 'fbx-clone.appspot.com',
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: MEASUREMENT_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID,
 });
 
 const db = firebaseApp.firestore();
